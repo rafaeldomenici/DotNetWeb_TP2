a2eb0fbe6c54443e5118a2e48ed72410181bb7b8
Rafael Domenici <rafaeldomenici@hotmail.com>
2024-03-04 13:39:32 -0300

    adicionando tp2

---
+++ rafaeldomenici/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace rafaeldomenici.Models
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string cpf;
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Idade { get => idade; set => idade = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+    }
+}
---
+++ rafaeldomenici/Pages/NovaPagina.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace rafaeldomenici.Pages
+{
+    public class NovaPaginaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
